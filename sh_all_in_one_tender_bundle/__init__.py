fd2bfdcf68b803cdd70205f5dfcff80d006054af
serviestiba <131922951+serviestiba@users.noreply.github.com>
2024-03-10 10:41:56 -0600

    Delete sh_all_in_one_tender_bundle/__init__.py

--- sh_all_in_one_tender_bundle/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Softhealer Technologies.
-
-from . import sh_po_tender_management
-from . import sh_po_tender_portal
-from . import sh_rfq_portal
-from . import sh_rfq_portal_signature
-from . import sh_vendor_signup
