b100109a1967464d4be927fc8dfe2b2ed192cbf4
oskr1087 <om@prisehub.com>
2024-06-25 10:38:44 -0500

    [REF] Method compute total subtotal not cancel status

--- serviestiba_extend/models/purchase_order.py
+++ serviestiba_extend/models/purchase_order.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, _
+from odoo import models, fields, api
 
 
 class PurchaseOrder(models.Model):
@@ -26,3 +26,20 @@ class PurchaseOrder(models.Model):
                 "state": "preapprove",
             }
         )
+
+    @api.depends_context("lang")
+    @api.depends(
+        "order_line.taxes_id",
+        "order_line.price_subtotal",
+        "amount_total",
+        "amount_untaxed",
+    )
+    def _compute_tax_totals(self):
+        for order in self:
+            order_lines = order.order_line.filtered(
+                lambda x: not x.display_type and x.status not in ["cancel"]
+            )
+            order.tax_totals = self.env["account.tax"]._prepare_tax_totals(
+                [x._convert_to_tax_base_line_dict() for x in order_lines],
+                order.currency_id or order.company_id.currency_id,
+            )
