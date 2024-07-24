6877cda53351d3e54a404d933627070a78c6481a
oskr1087 <om@prisehub.com>
2024-07-23 20:15:30 -0500

    [ADD] Module Serviestibas extend

---
+++ serviestiba_extend/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#                 @author PriseHub
+##############################################################################
+
+{
+    "name": "Serviestiba Extend Features",
+    "version": "16.0",
+    "description": """ Changes for Serviestiba Features
+    """,
+    "category": "Purchase",
+    "author": "PriseHub",
+    "website": "",
+    "depends": [
+        "purchase",
+        "sh_all_in_one_tender_bundle",
+    ],
+    "data": [
+        "views/purchase_order_view.xml",
+        "views/stock_picking_view.xml",
+        "views/purchase_template.xml",
+    ],
+    "application": False,
+    "installable": True,
+    "license": "OPL-1",
+}
