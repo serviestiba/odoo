2b0599164bec6f5eb4fcb9cc60ae379a4ffdc162
oskr1087 <om@prisehub.com>
2024-08-21 17:54:03 -0500

    [UP] Version module

--- serviestiba_extend/__manifest__.py
+++ serviestiba_extend/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "Serviestiba Extend Features",
-    "version": "16.0",
+    "version": "16.1",
     "description": """ Changes for Serviestiba Features
     """,
     "category": "Purchase",
