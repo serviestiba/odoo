f01a5a7e1475634402cb7bf874e4f8e42ca6bbdc
Oscar <oskr1087lema@gmail.com>
2025-12-19 10:16:52 -0500

    [FIX] Compute totals status cancel

--- serviestiba_extend/models/purchase_order.py
+++ serviestiba_extend/models/purchase_order.py
@@ -29,6 +29,24 @@ class PurchaseOrder(models.Model):
         )
 
 
+    @api.depends('order_line.price_subtotal', 'company_id', 'currency_id', 'order_line.status')
+    def _amount_all(self):
+        AccountTax = self.env['account.tax']
+        for order in self:
+            order_lines = order.order_line.filtered(lambda x: not x.display_type and not x.status == 'cancel')
+            base_lines = [line._prepare_base_line_for_taxes_computation() for line in order_lines]
+            AccountTax._add_tax_details_in_base_lines(base_lines, order.company_id)
+            AccountTax._round_base_lines_tax_details(base_lines, order.company_id)
+            tax_totals = AccountTax._get_tax_totals_summary(
+                base_lines=base_lines,
+                currency=order.currency_id or order.company_id.currency_id,
+                company=order.company_id,
+            )
+            order.amount_untaxed = tax_totals['base_amount_currency']
+            order.amount_tax = tax_totals['tax_amount_currency']
+            order.amount_total = tax_totals['total_amount_currency']
+            order.amount_total_cc = tax_totals['total_amount']
+
 
 class ShPurchaseAgreement(models.Model):
     _inherit = "purchase.agreement"
