4a600da963d23fb2cca745cbd391569add9beaad
Oscar <oskr1087lema@gmail.com>
2026-08-13 11:30:30 -0500

    [REF] Line PO states no create stock move

--- serviestiba_extend/models/purchase_order.py
+++ serviestiba_extend/models/purchase_order.py
@@ -28,13 +28,18 @@ class PurchaseOrder(models.Model):
             }
         )
 
-
-    @api.depends('order_line.price_subtotal', 'company_id', 'currency_id', 'order_line.status')
+    @api.depends(
+        "order_line.price_subtotal", "company_id", "currency_id", "order_line.status"
+    )
     def _amount_all(self):
-        AccountTax = self.env['account.tax']
+        AccountTax = self.env["account.tax"]
         for order in self:
-            order_lines = order.order_line.filtered(lambda x: not x.display_type and not x.status == 'cancel')
-            base_lines = [line._prepare_base_line_for_taxes_computation() for line in order_lines]
+            order_lines = order.order_line.filtered(
+                lambda x: not x.display_type and not x.status == "cancel"
+            )
+            base_lines = [
+                line._prepare_base_line_for_taxes_computation() for line in order_lines
+            ]
             AccountTax._add_tax_details_in_base_lines(base_lines, order.company_id)
             AccountTax._round_base_lines_tax_details(base_lines, order.company_id)
             tax_totals = AccountTax._get_tax_totals_summary(
@@ -42,10 +47,10 @@ class PurchaseOrder(models.Model):
                 currency=order.currency_id or order.company_id.currency_id,
                 company=order.company_id,
             )
-            order.amount_untaxed = tax_totals['base_amount_currency']
-            order.amount_tax = tax_totals['tax_amount_currency']
-            order.amount_total = tax_totals['total_amount_currency']
-            order.amount_total_cc = tax_totals['total_amount']
+            order.amount_untaxed = tax_totals["base_amount_currency"]
+            order.amount_tax = tax_totals["tax_amount_currency"]
+            order.amount_total = tax_totals["total_amount_currency"]
+            order.amount_total_cc = tax_totals["total_amount"]
 
 
 class ShPurchaseAgreement(models.Model):
@@ -57,3 +62,17 @@ class ShPurchaseAgreement(models.Model):
         default=fields.Datetime.now,
         readonly=True,
     )
+
+
+class PurchaseOrderLine(models.Model):
+    _inherit = "purchase.order.line"
+
+    def _create_stock_moves(self, picking):
+        values = []
+
+        for line in self.filtered(
+            lambda l: not l.display_type and l.status == "confirm"
+        ):
+            for val in line._prepare_stock_moves(picking):
+                values.append(val)
+        return self.env["stock.move"].create(values)
